9f7a912fd3757dece033df44843419f43f0a4a76
MD.JIHAD HOSSAIN <43439618+mdjihad560@users.noreply.github.com>
2022-09-05 11:24:15 +0600

    done: service page

--- src/js/main.js
+++ src/js/main.js
@@ -378,6 +378,40 @@ if(fugo_testimonial_slider2.is_exist()){
 }
 
 /*--------------------------------------------------------------
+PRICING TABLE JS INIT
+------------------------------------------------------------*/
+    // Table BTN Trigger
+    $("#l5-pricing-btn .toggle-btn").on("click", function (e) {
+      console.log($(e.target).parent().parent().hasClass("monthly-active"));
+      $(e.target).toggleClass("clicked");
+      if ($(e.target).parent().parent().hasClass("monthly-active")) {
+        $(e.target)
+          .parent()
+          .parent()
+          .removeClass("monthly-active")
+          .addClass("yearly-active");
+      } else {
+        $(e.target)
+          .parent()
+          .parent()
+          .removeClass("yearly-active")
+          .addClass("monthly-active");
+      }
+    });
+
+    $("[data-pricing-trigger]").on("click", function (e) {
+      $(e.target).addClass("active").siblings().removeClass("active");
+      var target = $(e.target).attr("data-target");
+      console.log($(target).attr("data-value-active") == "monthly");
+      if ($(target).attr("data-value-active") == "monthly") {
+        $(target).attr("data-value-active", "yearly");
+      } else {
+        $(target).attr("data-value-active", "monthly");
+      }
+    });
+
+
+/*--------------------------------------------------------------
 NEXTO SWIPER SLIDER JS INIT
 ------------------------------------------------------------*/
 var swiper_slider = $('.nexto-swiper-slider');
